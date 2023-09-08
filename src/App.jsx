f1a4ee65c2c53cd4d55e2c70339012c5d772d457
Frederick <pagadamf@msu.edu>
2023-09-08 16:22:40 -0400

    Setting up deployment

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Frederick Pagadam",
+  title: "Software Engineer & Musician",
+  email: "pagadamfrederick@gmail.com",
   gitHub: "microsoft",
-  instagram: "microsoft",
+  instagram: "Frederick Pagadam",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
